244694391bd82899601c95b8e265b57e74f69a7a
Omar Korim <omarkorim3210@gmail.com>
2024-12-01 20:34:15 +0200

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -1,49 +1,58 @@
-
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+    // Define the latitude and longitude of Lana Village, El Arish
     var myLatlng = new google.maps.LatLng(31.12981437334114, 33.814224865522284);
-    // 39.399872
-    // -8.224454    
-    
+
+    // Map options
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
         center: myLatlng,
-
-        // How you would like to style the map. 
         scrollwheel: false,
-        styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
+        styles: [
+            {"featureType": "administrative.land_parcel", "elementType": "all", "stylers": [{"visibility": "off"}]},
+            {"featureType": "landscape.man_made", "elementType": "all", "stylers": [{"visibility": "off"}]},
+            {"featureType": "poi", "elementType": "labels", "stylers": [{"visibility": "off"}]},
+            {"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "simplified"}, {"lightness": 20}]},
+            {"featureType": "road.highway", "elementType": "geometry", "stylers": [{"hue": "#f49935"}]},
+            {"featureType": "road.highway", "elementType": "labels", "stylers": [{"visibility": "simplified"}]},
+            {"featureType": "road.arterial", "elementType": "geometry", "stylers": [{"hue": "#fad959"}]},
+            {"featureType": "road.arterial", "elementType": "labels", "stylers": [{"visibility": "off"}]},
+            {"featureType": "road.local", "elementType": "geometry", "stylers": [{"visibility": "simplified"}]},
+            {"featureType": "road.local", "elementType": "labels", "stylers": [{"visibility": "simplified"}]},
+            {"featureType": "transit", "elementType": "all", "stylers": [{"visibility": "off"}]},
+            {"featureType": "water", "elementType": "all", "stylers": [{"hue": "#a1cdfc"}, {"saturation": 30}, {"lightness": 49}]}
+        ]
     };
 
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
+    // Get the map container element
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
+    // Create the Google Map
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['Brooklyn'];
 
+    // Add a marker with a tooltip for Lana Village, El Arish
+    new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: 'images/loc.png', // Optional custom icon
+        title: 'Lana Village, El Arish' // Tooltip text for the marker
+    });
+
+    // Example: Add other addresses dynamically (if needed)
+    var addresses = ['Brooklyn'];
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x] + '&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
                 icon: 'images/loc.png'
             });
-
         });
     }
-    
 }
+
+// Initialize the map when the page loads
 google.maps.event.addDomListener(window, 'load', init);
